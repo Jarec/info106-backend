61066f85151c2580546a7e57828884f45a048280
Jaroslav Crhonek <info106@info106.cz>
2020-10-25 18:27:02 +0100

    add CORS headers to all responses

--- src/institutions/search.js
+++ src/institutions/search.js
@@ -1,7 +1,9 @@
 const data = require("./index/data.json");
 const index = require("./index/index.json");
 
+const headers = require("../corsHeaders");
 const options = require("./index/config");
+
 const MiniSearch = require("minisearch");
 const removeAccents = require("remove-accents");
 
@@ -10,7 +12,7 @@ const miniSearch = MiniSearch.loadJS(index, options);
 module.exports.handle = async ({ queryStringParameters }) => {
   const query = queryStringParameters?.query;
   if (query == null) {
-    return { statusCode: 200, body: JSON.stringify([]) };
+    return { statusCode: 200, body: JSON.stringify([]), headers };
   }
   const limit = queryStringParameters?.limit ?? 10;
   const result = miniSearch
@@ -20,5 +22,6 @@ module.exports.handle = async ({ queryStringParameters }) => {
   return {
     statusCode: 200,
     body: JSON.stringify(result),
+    headers,
   };
 };
